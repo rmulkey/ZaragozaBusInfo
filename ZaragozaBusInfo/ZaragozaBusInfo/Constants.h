8f679b0f42db0d2d4f4a212b4aa4cee39a3c457d
Mulkey, Rodrigo S <rmulkey@gmail.com>
2016-05-02 16:25:37 -0500

    adding constants file with API endpoint;

---
+++ ZaragozaBusInfo/ZaragozaBusInfo/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  ZaragozaBusInfo
+//
+//  Created by Mulkey, Rodrigo S. on 5/2/16.
+//  Copyright © 2016 Mulkey, Rodrigo S. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+
+#endif /* Constants_h */
