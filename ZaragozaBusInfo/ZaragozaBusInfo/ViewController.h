e94161afa0fc8d7daa5c4f6733a39bf72e2ec1f5
Mulkey, Rodrigo S <rmulkey-acn@accenture.com>
2016-05-02 15:58:07 -0500

    initial commit;

---
+++ ZaragozaBusInfo/ZaragozaBusInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZaragozaBusInfo
+//
+//  Created by Mulkey, Rodrigo S. on 5/2/16.
+//  Copyright © 2016 Mulkey, Rodrigo S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZaragozaBusInfo/ZaragozaBusInfo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZaragozaBusInfo
+//
+//  Created by Mulkey, Rodrigo S. on 5/2/16.
+//  Copyright © 2016 Mulkey, Rodrigo S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
