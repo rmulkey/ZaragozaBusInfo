adc0bd2ae1596b7e55b33f404de6a95c62c9c468
Mulkey, Rodrigo S <rmulkey@gmail.com>
2016-05-03 03:07:28 -0500

    some more ui changes, using prototype cells

---
+++ ZaragozaBusInfo/ZaragozaBusInfo/BusListCell.h
@@ -0,0 +1,13 @@
+//
+//  BusListCell.h
+//  ZaragozaBusInfo
+//
+//  Created by Mulkey, Rodrigo S. on 5/3/16.
+//  Copyright © 2016 Mulkey, Rodrigo S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BusListCell : UITableViewCell
+
+@end
