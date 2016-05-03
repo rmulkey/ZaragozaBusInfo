2722e4f8b933768b2f29266e586e22dcb2436729
Mulkey, Rodrigo S <rmulkey@gmail.com>
2016-05-03 03:10:09 -0500

    adding tv required methods, populating cell

--- ZaragozaBusInfo/ZaragozaBusInfo/ViewController.m
+++ ZaragozaBusInfo/ZaragozaBusInfo/ViewController.m
@@ -7,21 +7,55 @@
 //
 
 #import "ViewController.h"
+#import "BusListCell.h"
+#import "RequestManager.h"
 
-@interface ViewController ()
-
-@end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.listOfBuses count];
 }
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    // Create and reuse cell
+    
+    static NSString *CellIdentifier = @"busListCell";
+    
+    BusListCell *cell = [tableView
+                              dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[BusListCell alloc]
+                initWithStyle:UITableViewCellStyleDefault
+                reuseIdentifier:CellIdentifier];
+    }
+    
+    //Populating cell
+    
+    NSDictionary *busValues = self.listOfBuses[indexPath.row];
+    
+    
+    cell.titleLabel.text = [NSString stringWithFormat:@"%@", busValues[@"title"]];
+    cell.subTitleLabel.text = [NSString stringWithFormat:@"%@", busValues[@"subtitle"]];
+
+    return cell;
+
+}
+
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 120.0f;
+}
+
 
 @end
